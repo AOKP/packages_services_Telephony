AOKP/packages_apps_Phone

Quiet Hours (3/3)
Change-Id: I7392ffcdb2363c54292d1783120fd1dde54448c4
Signed-off-by: sethyx <sethyx@gmail.com>

src/com/android/phone/Ringer.java
@@ -30,7 +30,10 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.util.Log;
+
+import java.util.Calendar;
 
 import com.android.internal.telephony.Phone;
 /**
@@ -162,7 +165,7 @@ public class Ringer {
             AudioManager audioManager =
                     (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
 
-            if (audioManager.getStreamVolume(AudioManager.STREAM_RING) == 0) {
+            if (audioManager.getStreamVolume(AudioManager.STREAM_RING) == 0 || inQuietHours()) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
@@ -340,5 +343,28 @@ public class Ringer {
 
     private static void log(String msg) {
         Log.d(LOG_TAG, msg);
+    }
+    
+    private boolean inQuietHours() {
+        boolean quietHoursEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_ENABLED, 0) != 0;
+        int quietHoursStart = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_START, 0);
+        int quietHoursEnd = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_END, 0);
+        boolean quietHoursRinger = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.QUIET_HOURS_RINGER, 0) != 0;
+        if (quietHoursEnabled && quietHoursRinger && (quietHoursStart != quietHoursEnd)) {
+            // Get the date in "quiet hours" format.
+            Calendar calendar = Calendar.getInstance();
+            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning.
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
+        }
+        return false;
     }
 }

AOKP/packages_services_Telephony

src/com/android/phone/Ringer.java
@@ -34,6 +34,8 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
+import java.util.Calendar;
+
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -170,7 +172,7 @@ public class Ringer {
             AudioManager audioManager =
                     (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
 
-            if (audioManager.getStreamVolume(AudioManager.STREAM_RING) == 0) {
+            if (audioManager.getStreamVolume(AudioManager.STREAM_RING) == 0 || inQuietHours()) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
@@ -353,5 +355,28 @@ public class Ringer {
 
     private static void log(String msg) {
         Log.d(LOG_TAG, msg);
+    }
+
+    private boolean inQuietHours() {
+        boolean quietHoursEnabled = Settings.AOKP.getInt(mContext.getContentResolver(),
+                Settings.AOKP.QUIET_HOURS_ENABLED, 0) != 0;
+        int quietHoursStart = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.AOKP.QUIET_HOURS_START, 0);
+        int quietHoursEnd = Settings.AOKP.getInt(mContext.getContentResolver(),
+                Settings.AOKP.QUIET_HOURS_END, 0);
+        boolean quietHoursRinger = Settings.AOKP.getInt(mContext.getContentResolver(),
+                Settings.AOKP.QUIET_HOURS_RINGER, 0) != 0;
+        if (quietHoursEnabled && quietHoursRinger && (quietHoursStart != quietHoursEnd)) {
+            // Get the date in "quiet hours" format.
+            Calendar calendar = Calendar.getInstance();
+            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
+            if (quietHoursEnd < quietHoursStart) {
+                // Starts at night, ends in the morning.
+                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
+            } else {
+                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
+            }
+        }
+        return false;
     }
 }