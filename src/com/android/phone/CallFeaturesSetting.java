omnirom/android_packages_services_Telephony

[4/4] Telephony: Smart Phone Call
Put on your ear, and this will automatically call current number in message or dialer

Change-Id: Ib7ae9b73af82b809aa782baf0b90e0c6cd4fc04e

src/com/android/phone/CallFeaturesSetting.java
@@ -197,6 +197,7 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String BUTTON_NON_INTRUSIVE_INCALL_KEY = "button_non_intrusive_incall";
     private static final String BUTTON_CALL_END_SOUND_KEY = "button_call_end_sound";
+    private static final String BUTTON_SMART_PHONE_CALL_KEY = "button_smart_phone_call";
 
     private Intent mContactListIntent;
 
@@ -285,6 +286,7 @@ public class CallFeaturesSetting extends PreferenceActivity
     private SipSharedPreferences mSipSharedPreferences;
     private CheckBoxPreference mNonIntrusiveInCall;
     private CheckBoxPreference mCallEndSound;
+    private CheckBoxPreference mSmartCall;
     private ListPreference mFlipAction;
     private CheckBoxPreference mEnableSuggestions;
     private CheckBoxPreference mEnableReverseLookup;
@@ -548,6 +550,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mNonIntrusiveInCall){
             Settings.System.putInt(getContentResolver(), Settings.System.NON_INTRUSIVE_INCALL,
                     mNonIntrusiveInCall.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mSmartCall){
+            Settings.System.putInt(getContentResolver(), Settings.System.SMART_PHONE_CALLER,
+                    mSmartCall.isChecked() ? 1 : 0);
             return true;
         } else if (preference == mEnableSuggestions){
             Settings.System.putInt(getContentResolver(), Settings.System.ENABLE_DIALER_SUGGESTIONS,
@@ -1683,10 +1689,14 @@ public class CallFeaturesSetting extends PreferenceActivity
         mNonIntrusiveInCall.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.NON_INTRUSIVE_INCALL, 1) == 0 ? false : true);
 
+        mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
+        mSmartCall.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.SMART_PHONE_CALLER, 0) != 0 ? true : false);
+
         mEnableSuggestions = (CheckBoxPreference) findPreference(BUTTON_ENABLE_SUGGESTIONS);
         mEnableSuggestions.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.ENABLE_DIALER_SUGGESTIONS, 1) == 0 ? false : true);
- 
+
         mEnableReverseLookup = (CheckBoxPreference) findPreference(BUTTON_ENABLE_REVERSE_LOOKUP);
         mEnableReverseLookup.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.ENABLE_DIALER_REVERSE_LOOKUP, 1) == 0 ? false : true);

AOKP/packages_services_Telephony

Change-Id: Idfad1406be80101ee87c5a150e01907bb304bbf9

src/com/android/phone/CallFeaturesSetting.java
@@ -198,6 +198,8 @@ public class CallFeaturesSetting extends PreferenceActivity
 
     private static final String BUTTON_NON_INTRUSIVE_INCALL_KEY = "button_non_intrusive_incall";
 
+    private static final String BUTTON_SMART_PHONE_CALL_KEY = "button_smart_phone_call";
+
     private Intent mContactListIntent;
 
     /** Event for Async voicemail change call */
@@ -284,6 +286,8 @@ public class CallFeaturesSetting extends PreferenceActivity
     private CheckBoxPreference mVoicemailNotificationVibrate;
     private SipSharedPreferences mSipSharedPreferences;
     private CheckBoxPreference mNonIntrusiveInCall;
+
+    private CheckBoxPreference mSmartCall;
 
     private class VoiceMailProvider {
         public VoiceMailProvider(String name, Intent intent) {
@@ -542,6 +546,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (preference == mNonIntrusiveInCall){
             Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.NON_INTRUSIVE_INCALL,
                     mNonIntrusiveInCall.isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mSmartCall){
+            Settings.AOKP.putInt(getContentResolver(), Settings.AOKP.SMART_PHONE_CALLER,
+                    mSmartCall.isChecked() ? 1 : 0);
             return true;
         }
         return false;
@@ -1643,6 +1651,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         mNonIntrusiveInCall = (CheckBoxPreference) findPreference(BUTTON_NON_INTRUSIVE_INCALL_KEY);
         mNonIntrusiveInCall.setChecked(Settings.AOKP.getInt(getContentResolver(),
                 Settings.AOKP.NON_INTRUSIVE_INCALL, 1) == 0 ? false : true);
+
+        mSmartCall = (CheckBoxPreference) findPreference(BUTTON_SMART_PHONE_CALL_KEY);
+        mSmartCall.setChecked(Settings.AOKP.getInt(getContentResolver(),
+                Settings.AOKP.SMART_PHONE_CALLER, 0) != 0 ? true : false);
 
         // create intent to bring up contact list
         mContactListIntent = new Intent(Intent.ACTION_GET_CONTENT);