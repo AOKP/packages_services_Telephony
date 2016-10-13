CyanogenMod/android_packages_services_Telephony

Telephony: Fix mobile network settings for CDMA without LTE
Change-Id: I789e30583195011c5f97884a9aa74f66ad90a0cd

src/com/android/phone/MobileNetworkSettings.java
@@ -1471,6 +1471,9 @@ public class MobileNetworkSettings extends PreferenceActivity
                             break;
                     }
                 }
+            } else {
+                ev[0] = com.android.phone.R.array.enabled_networks_cdma_choices;
+                ev[1] = com.android.phone.R.array.enabled_networks_cdma_values;
             }
         } else if (phoneType == PhoneConstants.PHONE_TYPE_GSM) {
             if (isSupportTdscdma(context, subId)) {

AOKP/packages_services_Telephony

src/com/android/phone/MobileNetworkSettings.java
@@ -1487,6 +1487,9 @@ public class MobileNetworkSettings extends PreferenceActivity
                             break;
                     }
                 }
+            } else {
+                ev[0] = com.android.phone.R.array.enabled_networks_cdma_no_lte_choices;
+                ev[1] = com.android.phone.R.array.enabled_networks_cdma_no_lte_values;
             }
         } else if (phoneType == PhoneConstants.PHONE_TYPE_GSM) {
             if (isSupportTdscdma(context, subId)) {